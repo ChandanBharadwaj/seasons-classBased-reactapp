f88dec80c97b345e306798b13dc30853954adfad
Chandan Bharadwaj <kolluruchandanbharadwaj@gmail.com>
2019-10-31 09:49:54 +0000

    geo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  );
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
